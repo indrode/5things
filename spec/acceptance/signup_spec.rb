51241ec5492b1b76bc96db9dd26e5ac29f2b0ee0
Indro De <indro.de@gmail.com>
2013-03-22 12:27:34 +0100

    Begin signup acceptance specs

--- spec/acceptance/signup_spec.rb
+++ spec/acceptance/signup_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
+# extract into elsewhere
+def visit_root
+  visit ('/')
+end
+
 describe "Signing Up" do
-  describe "New user signup" do
+  describe "a new user" do
+    it "should be able to navigate to registration page" do
+      visit_root
+      click_link('sign_up_now')
+      page.should have_content('Registration')
+    end
+
     it "should allow a user to sign up"
     it "should add a new user"
     it "should generate a new tasklist for the user"
