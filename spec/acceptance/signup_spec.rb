0d3321ce7d779b63043632c47a320265620ca94a
Indro De <indro.de@gmail.com>
2013-03-22 13:25:04 +0100

    Adjust specs

--- spec/acceptance/signup_spec.rb
+++ spec/acceptance/signup_spec.rb
@@ -5,15 +5,21 @@ def visit_root
   visit ('/')
 end
 
-describe "Signing Up" do
-  describe "a new user" do
+describe "Signup" do
+  context "New user" do
     it "should be able to navigate to registration page" do
       visit_root
       click_link('sign_up_now')
       page.should have_content('Registration')
     end
 
-    it "should allow a user to sign up"
+    it "should be able to sign up" do
+      visit('/users/new')
+      fill_in('user_email', :with => 'test@5thingsapp.com')
+      click_button('Register')
+      page.should have_content('Your account has been created!')
+    end
+
     it "should add a new user"
     it "should generate a new tasklist for the user"
     it "should send out an activation confirmation"  
