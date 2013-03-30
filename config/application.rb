1817e203f034ea7beb7975a18304c0524bdc5f62
Indro De <indro.de@gmail.com>
2013-03-30 15:27:15 +0100

    Revert "Add apps/modules to load path"
    This reverts commit 9fe5ee45b05e76c1b7b18b323cd12bfd7244b29d.

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Fivethings
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/app/modules)
+    # config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
